1b9f21c96751aa2560e5c5a2f9e29ef9889ea9e3
Akhilesh Tripathi <112152867+Akhilesh0101@users.noreply.github.com>
2023-08-20 00:32:24 +0530

    Update VideoContainer.js

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -8,24 +8,38 @@ const VideoContainer = () => {
   
 
   const[videos , setVideos]= useState([])
+  const[nextPageToken , setnextPageToken]  = useState("")
    
   useEffect(()=>{
    getVideos();
   },[]);
 
+  useEffect(()=>{
+    window.addEventListener("scroll",infiniteScroll, true);
+    return()=>{
+      window.removeEventListener("scroll,infiniteScroll", true);
+    }
+  },[nextPageToken]);
 
   const getVideos = async ()=>{
-    const data= await fetch(YOUTUBE_VIDEO_API);
+    const url = nextPageToken !== "" ? `${YOUTUBE_VIDEO_API}&pageToken=${nextPageToken}` : YOUTUBE_VIDEO_API;
+    const data= await fetch(url);
     const json = await data.json();
-    
+    setnextPageToken(json?.nextPageToken)
     setVideos(json.items)
+  }
+
+  const infiniteScroll = ()=>{
+    if(window.innerHeight+ Math.round(document.documentElement.scrollTop)>=document.documentElement.offsetHeight - 300){
+      getVideos();
+    }
   }
   
   if (!videos) return null;
   return (videos.length===0)?(<Shimmar/>)
   :(
    
-    <div className='flex w-full flex-wrap ml-8 '>
+    <div className='flex w-full flex-wrap ml-8  '>
       {videos.map((videos)=>(
       <Link key={videos.id} to={"/watch?v="+videos.id}><VideoCard  info={videos}/>
       </Link>
